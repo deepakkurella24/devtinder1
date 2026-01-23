0481d7c0763438101306d0a85f02dfa9630e1213
deepakkurella24 <deepak.kurella24@sasi.ac.in>
2026-01-23 18:28:27 +0530

    creating users collection

--- src/app.js
+++ src/app.js
@@ -1,7 +1,19 @@
 const express=require('express')
 const {connectDB}=require('./config/database')
+const User=require('./models/user')
 const app=express();
 
+app.post('/signUp',async (req,res)=>{
+    const user=new User({
+        firstName:'deepak',
+        lastName:'kurella',
+        email:'deepak.kurella24@sasi.ac.in',
+        password:'helloworld@123'
+    })
+    await user.save();
+    console.log('user added successfully')
+    res.send('added succesfully')
+})
 
 connectDB().then((res)=>{
     console.log('DB connected succesfully')
